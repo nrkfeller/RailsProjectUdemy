902000e630c2eeb7ec3fdde0446c4336d9461180
nrkfeller <nrkfeller@gmail.com>
2015-02-11 05:21:44 +0000

    Assiciation between comments and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
   validates_presence_of :title, :date, :description #validation
-  
+  has_many :comments, :dependent => :destroy #destroy loads an instance and destroy the instance method, and this invokes the activities to remove the dependences. would not be invoked if we used delete. essentially removes the opportunity for bugging
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211044142) do
+ActiveRecord::Schema.define(version: 20150211050721) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "event_id"
+    t.text     "details"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.string   "title"
